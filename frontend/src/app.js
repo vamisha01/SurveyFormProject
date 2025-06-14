385d5a7c059aa0e3d0855a4d4e6eb0e2a20c7f3a
vamisha01 <amishaupadhyay02@gmail.com>
2025-06-14 14:08:32 +0530

    Create app.js

---
+++ frontend/src/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SurveyForm from './components/SurveyForm';
+
+function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>Survey Form</h2>
+      <SurveyForm />
+    </div>
+  );
+}
+
+export default App;
